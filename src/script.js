17cd1b24d60c9bf826c19ad6294600bac2f3a255
bxity <boityy.js@gmail.com>
2023-08-23 17:40:34 +0200

    improvements to html

--- src/script.js
+++ src/script.js
@@ -193,41 +193,37 @@ function updateWeatherParameters(response) {
 const searchForm = document.getElementById("search-form");
 searchForm.addEventListener("submit", getWeatherData);
 
+
+const weatherIconsMap = {
+  "01d": "owf owf-800",
+  "02d": "owf owf-801",
+  "03d": "owf owf-803",
+  "04d": "owf owf-804",
+  "09d": "owf owf-502",
+  "10d": "owf owf-501",
+  "11d": "owf owf-200",
+  "13d": "owf owf-600",
+  "50d": "owf owf-741",
+
+};
+
+
 function updateWeatherIcon(conditionCode) {
   const iconElement = document.getElementById("weather-icon");
-  const weatherIconsMap = {
-    "01d": "wi-day-sunny",
-    "02d": "wi-day-cloudy",
-    "03d": "wi-cloud",
-    "04d": "wi-cloudy",
-    "09d": "wi-day-showers",
-    "10d": "wi-day-rain",
-    "11d": "wi-day-thunderstorm",
-    "13d": "wi-day-snow",
-    "50d": "wi-day-fog",
-  };
-  
-  const defaultIconClass = "wi-day-sunny";
-  
-  const iconClass = weatherIconsMap[conditionCode] || defaultIconClass;
-  
-  iconElement.className = "wi " + iconClass;
+  const iconClass = weatherIconsMap[conditionCode] || "owf owf-800"; 
+
+
+  iconElement.className = iconClass;
 }
 
-function updateWeatherData(weatherData) {
-  console.log("Weather Condition Code:", weatherData.weather[0].icon);
-  updateWeatherIcon(conditionCode);
-  
-}
 
-axios
-  .get(
-    "https://api.openweathermap.org/data/2.5/weather?q=${userCity}&appid=${apiKey}&units=${unit}"
-  )
-  .then((response) => {
-    const weatherData = response.data;
-    updateWeatherData(weatherData);
-  })
-  .catch((error) => {
-    console.error("Error fetching weather data:", error);
-  });
+const apiResponse = {
+  weather: [
+    {
+      icon: "01d" 
+    }
+  ]
+};
+
+
+updateWeatherIcon(apiResponse.weather[0].icon);
